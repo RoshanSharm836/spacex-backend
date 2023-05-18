51b0ffa0aedd59e565277cfe0d3184131dca82db
Roshanrocked <roshna837kumar@gmail.com>
2023-05-18 06:32:50 +0530

    done

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 mongoose.set("strictQuery", true);
 const connection = (mongoose.connection = async () => {
-  await mongoose.connect(process.env.DataBase_url);
+  await mongoose.connect(
+    "mongodb+srv://roshan836sharma:507JdZ5H3wOwrIYs@cluster0.4hplwyh.mongodb.net/"
+  );
 });
 
 module.exports = connection;
